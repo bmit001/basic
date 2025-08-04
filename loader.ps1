21519ba43636f52ad6802a72a35838e11c76e498
bmit001 <banumukhtarit@gmail.com>
2025-08-04 16:32:48 +0500

    Add files via upload

--- loader.ps1
+++ loader.ps1
@@ -1,5 +1,5 @@
 # Network share path where your installer is located
-$networkPath = "\\192.168.1.100\Installers\BASICS.bat"
+$networkPath = "\\"\\192.168.14.11\e\E\BASIC INSTALLATION\BASIC INSTALLATION\BASICS\BASICS.bat""
 
 Write-Host "Launching installer from $networkPath ..." -ForegroundColor Cyan
 Start-Process "cmd.exe" -ArgumentList "/c `"$networkPath`""
