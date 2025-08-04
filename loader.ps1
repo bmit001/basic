fab5d15bc7ab4e7ccf7ac75881aef7374ffc3073
bmit001 <banumukhtarit@gmail.com>
2025-08-04 16:41:59 +0500

    loader.ps1

--- loader.ps1
+++ loader.ps1
@@ -1,3 +1,11 @@
-# Loader script to run the batch installer from network path
-$networkPath = "\\192.168.14.11\e\E\BASIC INSTALLATION\BASIC INSTALLATION\BASICS\BASICS.bat"
-Start-Process -FilePath $networkPath -Wait
+# Loader.ps1 - Launch BASICS.bat from network share
+
+$path = "\\192.168.14.11\e\E\BASIC INSTALLATION\BASIC INSTALLATION\BASICS\BASICS.bat"
+
+if (Test-Path $path) {
+    Write-Host "Launching installer from network share..."
+    Start-Process -FilePath $path -Wait
+} else {
+    Write-Host "? Network path not found: $path"
+    Pause
+}
